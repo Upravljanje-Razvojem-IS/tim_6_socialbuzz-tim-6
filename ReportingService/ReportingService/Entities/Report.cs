f6772f764b6f75a6fc04ceec2fa6dc30c400e75f
acimovicTamara <dimitrije.acimovic04@gmail.com>
2021-07-25 23:04:18 +0200

    Create entity

---
+++ ReportingService/ReportingService/Entities/Report.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.ComponentModel.DataAnnotations;
+
+namespace ReportingService.Entities
+{
+    public class Report
+    {
+        [Key]
+        public Guid Id { get; set; }
+        [Required]
+        public Guid PostId { get; set; }
+        [Required]
+        public int NumberOfLikes { get; set; }
+        [Required]
+        public int NumberOfComments { get; set; }
+        [Required]
+        public int NumberOfMarks { get; set; }
+    }
+}
